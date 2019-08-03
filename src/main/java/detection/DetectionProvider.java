ad420b0725838b320d970542fcb9d1e1512242c3
Oiwane <hm16007@student.miyazaki-u.ac.jp>
2019-08-03 20:32:48 +0900

    ディレクトリを用意した

---
+++ src/main/java/detection/DetectionProvider.java
@@ -0,0 +1,16 @@
+//package detection;
+//
+//import com.intellij.codeInspection.InspectionToolProvider;
+//import com.intellij.psi.impl.source.tree.java.PsiKeywordImpl;
+//import org.jetbrains.annotations.NotNull;
+//
+//public class DetectionProvider implements InspectionToolProvider {
+//
+//  @NotNull
+//  @Override
+//  public Class[] getInspectionClasses() {
+//    return new Class[]{
+//
+//    }
+//  }
+//}
