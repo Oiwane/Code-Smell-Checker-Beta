12c1f6e40c9d2c7a4bef89f210885f4d6fee8271
Oiwane <hm16007@student.miyazaki-u.ac.jp>
2019-09-11 02:25:37 +0900

    未完成

--- src/main/java/inspection/MessageChains/MessageChainsInspection.java
+++ src/main/java/inspection/MessageChains/MessageChainsInspection.java
@@ -4,7 +4,8 @@ import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.*;
-import inspection.Setting;
+import inspection.InspectionSetting;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class MessageChainsInspection extends AbstractBaseJavaLocalInspectionTool {
@@ -12,14 +13,25 @@ public class MessageChainsInspection extends AbstractBaseJavaLocalInspectionTool
   private int numChains;
 
   public MessageChainsInspection() {
-    numChains = Setting.numChains;
-    System.out.println("MessageChainsInspection start");
+    numChains = InspectionSetting.numChains;
+    // System.out.println("MessageChainsInspection start");
   }
 
   @Override
   @NotNull
   public String getDisplayName() {
       return "Message chains";
+  }
+
+  @NonNls
+  @NotNull
+  public String getShortName() {
+    return "MessageChainsInspection";
+  }
+
+  @NotNull
+  public String getGroupDisplayName() {
+    return "Code Smell";
   }
 
   private void registerError(ProblemsHolder holder, PsiElement element) {
@@ -41,7 +53,7 @@ public class MessageChainsInspection extends AbstractBaseJavaLocalInspectionTool
           if (element instanceof PsiReferenceExpression) count++;
         }
 
-        if (count < numChains) return;
+        if (count <= numChains) return;
 
         registerError(holder, statement);
       }
