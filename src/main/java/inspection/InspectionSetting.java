6b53906bbd2dc389e24259efe2d0cf6b2360b7b8
Oiwane <hm16007@student.miyazaki-u.ac.jp>
2019-09-10 23:01:31 +0900

    インスペクタの設定用クラスの名前を変更

--- src/main/java/inspection/InspectionSetting.java
+++ src/main/java/inspection/InspectionSetting.java
@@ -1,8 +1,8 @@
 package inspection;
 
 public class Setting {
-  public static int numParameterList = 1;
-  public static int numLines = 10;
+  public static int numParameterList = 5;
+  public static int numLines = 5;
   public static int numChains = 3;
 
 }
