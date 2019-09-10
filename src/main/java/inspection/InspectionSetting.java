8b346a16b500accf4c9398211ea265e65621b5ce
Oiwane <hm16007@student.miyazaki-u.ac.jp>
2019-09-11 02:25:12 +0900

    インスペクタの設定用クラスを作成

--- src/main/java/inspection/InspectionSetting.java
+++ src/main/java/inspection/InspectionSetting.java
@@ -1,6 +1,6 @@
 package inspection;
 
-public class Setting {
+public class InspectionSetting {
   public static int numParameterList = 5;
   public static int numLines = 5;
   public static int numChains = 3;
