edd330754c1219aea39cbded10b2bf69e8b2641a
Oiwane Yuki <drhv8272@gmail.com>
2020-05-24 16:18:01 +0900

    inspectionパッケージのテストコードを作成・追加した

--- src/test/java/inspection/codeSmell/LongParameterListInspectionTest.java
+++ src/test/java/inspection/codeSmell/LongParameterListInspectionTest.java
@@ -1,8 +1,18 @@
 package inspection.codeSmell;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiParameterList;
+import com.intellij.psi.PsiType;
 import inspection.CodeSmellInspectionTest;
 
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class LongParameterListInspectionTest extends CodeSmellInspectionTest {
@@ -11,16 +21,55 @@ public class LongParameterListInspectionTest extends CodeSmellInspectionTest {
 
     @Override
     public void testForInspection() {
-        final LongParameterListInspection longParameterListInspection = new LongParameterListInspection();
+        final LongParameterListInspection inspection = new LongParameterListInspection();
 
         myFixture.configureByFile(originalFilePath);
-        myFixture.enableInspections(longParameterListInspection);
+        myFixture.enableInspections(inspection);
 
-        List<HighlightInfo> highlightInfoList = getHighlightInfoList(longParameterListInspection.getShortName());
+        List<HighlightInfo> highlightInfoList = getHighlightInfoList(inspection.getShortName());
 
         HighlightInfo highlightInfo = highlightInfoList.get(0);
 
         assertEquals(573, highlightInfo.getActualStartOffset());
         assertEquals(686, highlightInfo.getActualEndOffset());
+    }
+
+    public void testGetDisplayName() {
+        LongParameterListInspection inspection = new LongParameterListInspection();
+        assertEquals("Long parameter list", inspection.getDisplayName());
+    }
+
+    public void testGetShortName() {
+        LongParameterListInspection inspection = new LongParameterListInspection();
+        assertEquals("LongParameterListInspection", inspection.getShortName());
+    }
+
+    public void testCreateOptionsPanel() {
+        LongParameterListInspection inspection = new LongParameterListInspection();
+        JComponent component = inspection.createOptionsPanel();
+        JPanel panel = (JPanel) component.getComponent(0);
+        JLabel label = (JLabel) panel.getComponent(0);
+        String description = "detected length of \"" + inspection.getDisplayName() + "\"";
+        assertEquals(description, label.getText());
+    }
+
+    public void testCheckParameterList() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        LongParameterListInspection inspection = new LongParameterListInspection();
+        InspectionManager manager = InspectionManager.getInstance(getProject());
+        Method method = LongParameterListInspection.class.getDeclaredMethod("checkParameterList", PsiParameterList.class, InspectionManager.class, boolean.class);
+        assert method != null;
+        method.setAccessible(true);
+
+        PsiElementFactory factory = PsiElementFactory.getInstance(getProject());
+        String[] names = {""};
+        PsiType[] types = {PsiType.VOID};
+        PsiParameterList parameterList = factory.createParameterList(names, types);
+
+        ProblemDescriptor[] descriptors = (ProblemDescriptor[]) method.invoke(inspection, parameterList, manager, true);
+        assertNull(descriptors);
+    }
+
+    public void testBuildVisitor() {
+        /* testForInspectionにて使用しているのでテストはスルー */
     }
 }
--- src/test/java/inspection/codeSmell/MessageChainsInspectionTest.java
+++ src/test/java/inspection/codeSmell/MessageChainsInspectionTest.java
@@ -1,8 +1,19 @@
 package inspection.codeSmell;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.psi.PsiCodeBlock;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiExpression;
+import com.intellij.psi.PsiStatement;
 import inspection.CodeSmellInspectionTest;
 
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class MessageChainsInspectionTest extends CodeSmellInspectionTest {
@@ -12,13 +23,13 @@ public class MessageChainsInspectionTest extends CodeSmellInspectionTest {
     @Override
     public void testForInspection() {
         // 検出基準値の設定はしない
-        final MessageChainsInspection messageChainsInspection = new MessageChainsInspection();
+        final MessageChainsInspection inspection = new MessageChainsInspection();
 
         myFixture.configureByFile(originalFilePath);
-        myFixture.enableInspections(messageChainsInspection);
+        myFixture.enableInspections(inspection);
 
         // 『メッセージの連鎖』コードインスペクションが検出したコードを特定
-        List<HighlightInfo> highlightInfoList = getHighlightInfoList(messageChainsInspection.getShortName());
+        List<HighlightInfo> highlightInfoList = getHighlightInfoList(inspection.getShortName());
 
         // ハイライト部分の情報を取得
         // 今回のテストケースであれば、検出結果としては1つのみ
@@ -26,5 +37,50 @@ public class MessageChainsInspectionTest extends CodeSmellInspectionTest {
         // ハイライトすべき部分が正しいか確認
         assertEquals(346, highlightInfo.getActualStartOffset());
         assertEquals(388, highlightInfo.getActualEndOffset());
+    }
+
+    public void testGetDisplayName() {
+        MessageChainsInspection inspection = new MessageChainsInspection();
+        assertEquals("Message chains", inspection.getDisplayName());
+    }
+
+    public void testGetShortName() {
+        MessageChainsInspection inspection = new MessageChainsInspection();
+        assertEquals("MessageChainsInspection", inspection.getShortName());
+    }
+
+    public void testCreateOptionsPanel() {
+        MessageChainsInspection inspection = new MessageChainsInspection();
+        JComponent component = inspection.createOptionsPanel();
+        JPanel panel = (JPanel) component.getComponent(0);
+        JLabel label = (JLabel) panel.getComponent(0);
+        String description = "detected length of \"" + inspection.getDisplayName() + "\"";
+        assertEquals(description, label.getText());
+    }
+
+    public void testCheckExpression() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        MessageChainsInspection inspection = new MessageChainsInspection();
+        InspectionManager manager = InspectionManager.getInstance(getProject());
+        Method method = MessageChainsInspection.class.getDeclaredMethod("checkExpression", PsiExpression.class, InspectionManager.class, boolean.class);
+        assert method != null;
+        method.setAccessible(true);
+
+        // 上限値以下の時
+        PsiElementFactory factory = PsiElementFactory.getInstance(getProject());
+        PsiStatement statement = factory.createStatementFromText("System.out.println(\"test\");", null);
+        PsiExpression expression = (PsiExpression) statement.getChildren()[0];  // PsiMethodCallExpression
+        PsiCodeBlock codeBlock = factory.createCodeBlock();
+        codeBlock.add(expression);
+        ProblemDescriptor[] descriptors = (ProblemDescriptor[]) method.invoke(inspection, expression, manager, true);
+        assertNull(descriptors);
+
+        // 親のPsiElementがPsiExpression、もしくは継承したものの時
+        expression = (PsiExpression) statement.getChildren()[0];  // PsiReferenceExpression
+        descriptors = (ProblemDescriptor[]) method.invoke(inspection, expression, manager, true);
+        assertNull(descriptors);
+    }
+
+    public void testBuildVisitor() {
+        /* testForInspectionにて使用しているのでテストはスルー */
     }
 }
