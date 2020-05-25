4daa9915237a95c865abfc1dc1c4b7dfc2d6c7de
Oiwane Yuki <drhv8272@gmail.com>
2020-05-26 00:42:16 +0900

    ui.refactoringパッケージのテストコードを追加した

---
+++ src/test/java/ui/refactoring/replaceTempWithQuery/SelectTargetTempDialogTest.java
@@ -0,0 +1,13 @@
+package ui.refactoring.replaceTempWithQuery;
+
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+
+import java.util.ArrayList;
+
+public class SelectTargetTempDialogTest extends LightJavaCodeInsightFixtureTestCase {
+
+    public void testDialogTitle() {
+        SelectTargetTempDialog dialog = new SelectTargetTempDialog(getProject(), true, new ArrayList<>());
+        assertEquals("Select Temps", dialog.getTitle());
+    }
+}
